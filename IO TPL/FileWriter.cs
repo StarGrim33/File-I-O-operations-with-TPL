66ada2f321ca0cd0238f6dfc7a8fb138f2741c88
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-11-21 13:09:00 +0300

    Added TPL

---
+++ IO TPL/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IO_TPL
+{
+    public class FileWriter
+    {
+        public static async Task WriteFile(string filePath, string data)
+        {
+            using StreamWriter writer = new(filePath);
+            await writer.WriteAsync(data);
+            Console.WriteLine("File written");
+        }
+    }
+
+    public class FileReader
+    {
+        public static async Task<string> ReadFile(string filePath)
+        {
+            using StreamReader reader = new(filePath);
+            string content = await reader.ReadToEndAsync();
+            return content;
+        }
+    }
+}
---
+++ IO TPL/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IO_TPL
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            string filePath = @"C:\Users\Влад и Аня\Desktop\C# course\File-I-O-operations-with-TPL\example.txt";
+            string dataExample = "Vlad from Russia, Vladimir city";
+
+            await FileWriter.WriteFile(filePath, dataExample);
+            Task<string> readTask = FileReader.ReadFile(filePath);
+            await readTask;
+
+            Console.WriteLine($"File content: {readTask.Result}");
+        }
+    }
+}
